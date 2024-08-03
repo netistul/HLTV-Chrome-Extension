915ffdd192620cb395e98f9ea8f47b090522491f
netistul <tyiby@yahoo.com>
2024-08-03 18:51:25 +0300

    Fixed counting of live matches

--- background.js
+++ background.js
@@ -1,88 +1,98 @@
-// background.js
 function fetchDataAndStore() {
-  console.log('Attempting to fetch data...');
-  
-  fetch(`https://example.blob.core.windows.net/hltv/matches.json?timestamp=${new Date().getTime()}`)
-    .then(response => {
+  console.log("Attempting to fetch data...");
+
+  fetch(
+    `https://example.blob.core.windows.net/hltv/matches.json?timestamp=${new Date().getTime()}`
+  )
+    .then((response) => {
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
       return response.json();
     })
-    .then(data => {
-      console.log('Data fetched:', data);
+    .then((data) => {
+      console.log("Data fetched:", data);
       chrome.storage.local.set({ matchesData: data }, () => {
-        console.log('Data fetched and stored.');
-        
+        console.log("Data fetched and stored.");
+
         // Get the current date and time
         const now = new Date();
-        
-        // Filter live matches to count only those with a recordDate within the last hour
-        const liveMatchesCount = data.filter(match => {
-          if (match.date !== "Date not specified") {
-            return false; // Exclude matches with a specified date
-          } else {
+
+        // Filter live matches to count only those with a recordDate within the appropriate window
+        const liveMatches = data.filter((match) => {
+          if (match.date === "Date not specified") {
             const recordDate = new Date(match.recordDate);
-            const hoursDiff = (now - recordDate) / (3600000); // Convert milliseconds to hours
-            return hoursDiff <= 1; // Include if recordDate is less than or equal to 1 hour old
+            const hoursDiff = (now - recordDate) / 3600000; // Convert milliseconds to hours
+            return hoursDiff <= 5; // Include if recordDate is less than or equal to 5 hours old
           }
-        }).length;
-        
+          return false;
+        });
+
+        const upcomingMatches = data.filter((match) => {
+          if (match.date !== "Date not specified") {
+            const matchDate = new Date(match.date);
+            return now <= matchDate && matchDate - now <= 3600000; // Match within the next 1 hour
+          }
+          return false;
+        });
+
+        const liveMatchesCount = liveMatches.length;
+        const upcomingMatchesCount = upcomingMatches.length;
+
         // Update the badge
         if (liveMatchesCount > 0) {
-          chrome.action.setBadgeText({text: liveMatchesCount.toString()});
-          chrome.action.setBadgeBackgroundColor({color: '#2b6ea4'});
+          chrome.action.setBadgeText({ text: liveMatchesCount.toString() });
+          chrome.action.setBadgeBackgroundColor({ color: "#2b6ea4" });
         } else {
-          chrome.action.setBadgeText({text: ''}); // Clear badge when liveMatchesCount is 0
+          chrome.action.setBadgeText({ text: "" }); // Clear badge when liveMatchesCount is 0
         }
-        
+
         // Send a message to popup.js to let it know that new data is available
-        chrome.runtime.sendMessage({message: 'data_updated'}, function(response) {
-          if (chrome.runtime.lastError) {
-            console.warn(chrome.runtime.lastError.message);
-          } else {
-            // Handle the response or do other things.
+        chrome.runtime.sendMessage(
+          { message: "data_updated" },
+          function (response) {
+            if (chrome.runtime.lastError) {
+              console.warn(chrome.runtime.lastError.message);
+            } else {
+              // Handle the response or do other things.
+            }
           }
-        });
+        );
       });
     })
-    .catch(error => {
-      console.error('Error fetching data:', error);
+    .catch((error) => {
+      console.error("Error fetching data:", error);
     });
 }
 
-
 // Event listener for the install event
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installing.');
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installing.");
   fetchDataAndStore();
 });
 
 // Event listener for the activate event
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker activated.');
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activated.");
   fetchDataAndStore();
 });
 
 // Create an alarm that fires every 10 minute
-chrome.alarms.get('fetchDataAlarm', (alarm) => {
+chrome.alarms.get("fetchDataAlarm", (alarm) => {
   if (!alarm) {
-    chrome.alarms.create('fetchDataAlarm', { periodInMinutes: 10 });
+    chrome.alarms.create("fetchDataAlarm", { periodInMinutes: 10 });
   }
 });
-
 
 // Set up an alarm listener
 chrome.alarms.onAlarm.addListener((alarm) => {
-  console.log('Alarm fired:', alarm.name);
-  if (alarm.name === 'fetchDataAlarm') {
+  console.log("Alarm fired:", alarm.name);
+  if (alarm.name === "fetchDataAlarm") {
     fetchDataAndStore();
   }
 });
-
 
 // fetch data when Chrome starts up
 chrome.runtime.onStartup.addListener(() => {
   fetchDataAndStore();
 });
-
--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const matchList = document.getElementById("match-list");
 
   // Attempt to load cached HTML content first
-  const cachedHTML = localStorage.getItem('cachedMatchListHTML');
+  const cachedHTML = localStorage.getItem("cachedMatchListHTML");
   if (cachedHTML) {
     matchList.innerHTML = cachedHTML;
     // Re-initialize any JavaScript-based UI enhancements here, if necessary
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
   // Function to update data
   function updateData() {
     // Retrieve data from Chrome's storage
-    chrome.storage.local.get('matchesData', function(result) {
+    chrome.storage.local.get("matchesData", function (result) {
       const data = result.matchesData;
 
       if (!data) {
@@ -24,30 +24,44 @@ document.addEventListener("DOMContentLoaded", function() {
       const limitedData = data.slice(0, 20);
       const now = new Date(); // Use for both date comparison and recordDate comparison
 
-      const filteredMatches = limitedData.filter(match => {
+      const filteredMatches = limitedData.filter((match) => {
         if (match.date === "Date not specified") {
           const recordDate = new Date(match.recordDate);
-          const hoursDiff = (now - recordDate) / (3600000); // Convert milliseconds to hours
-          return hoursDiff <= 1; // Only include if recordDate is less than or equal to 1 hour old
+          const hoursDiff = (now - recordDate) / 3600000; // Convert milliseconds to hours
+          return hoursDiff <= 5; // Only include if recordDate is less than or equal to 5 hours old
         } else {
           const matchDate = new Date(match.date);
-          return now <= matchDate && (matchDate - now) <= 86400000; // Match within the next 24 hours
+          return now <= matchDate && matchDate - now <= 86400000; // Match within the next 24 hours
         }
       });
 
       if (filteredMatches.length === 0) {
-        matchList.innerHTML = "<p>No matches today or upcoming live matches within the last hour.</p>";
+        matchList.innerHTML =
+          "<p>No matches today or upcoming live matches within the last hour.</p>";
         return;
       }
 
-      matchList.innerHTML = filteredMatches.map(match => {
-        const team1Logo = (match.team1Logo && match.team1Logo !== "Logo not available") ? match.team1Logo : 'https://www.hltv.org/img/static/team/placeholder.svg';
-        const team2Logo = (match.team2Logo && match.team2Logo !== "Logo not available") ? match.team2Logo : 'https://www.hltv.org/img/static/team/placeholder.svg';
-        const eventName = match.event ? match.event : "Unknown Event";
-        const localTime = match.date === "Date not specified" ? "Live" : new Date(match.date).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-        const matchLink = match.matchLink ? match.matchLink : "#";
+      matchList.innerHTML = filteredMatches
+        .map((match) => {
+          const team1Logo =
+            match.team1Logo && match.team1Logo !== "Logo not available"
+              ? match.team1Logo
+              : "https://www.hltv.org/img/static/team/placeholder.svg";
+          const team2Logo =
+            match.team2Logo && match.team2Logo !== "Logo not available"
+              ? match.team2Logo
+              : "https://www.hltv.org/img/static/team/placeholder.svg";
+          const eventName = match.event ? match.event : "Unknown Event";
+          const localTime =
+            match.date === "Date not specified"
+              ? "Live"
+              : new Date(match.date).toLocaleTimeString([], {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                });
+          const matchLink = match.matchLink ? match.matchLink : "#";
 
-        return `
+          return `
           <a href="${matchLink}" target="_blank" class="list-group-item list-group-item-dark">
             <div class="d-flex w-100 justify-content-between">
               <div>
@@ -65,10 +79,11 @@ document.addEventListener("DOMContentLoaded", function() {
             </div>
           </a>
         `;
-      }).join('');
+        })
+        .join("");
 
       // Cache the generated HTML in local storage
-      localStorage.setItem('cachedMatchListHTML', matchList.innerHTML);
+      localStorage.setItem("cachedMatchListHTML", matchList.innerHTML);
 
       // Get the scrollHeight of the element after populating it
       const contentHeight = matchList.scrollHeight;
@@ -88,12 +103,14 @@ document.addEventListener("DOMContentLoaded", function() {
   updateData();
 
   // Listen for messages from background.js
-  chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      if (request.message === 'data_updated') {
-        // Fetch new data from chrome.storage.local
-        updateData();
-      }
+  chrome.runtime.onMessage.addListener(function (
+    request,
+    sender,
+    sendResponse
+  ) {
+    if (request.message === "data_updated") {
+      // Fetch new data from chrome.storage.local
+      updateData();
     }
-  );
+  });
 });
